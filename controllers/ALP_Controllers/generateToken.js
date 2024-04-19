2f62cb3d9019d7f01f58753a13712288879f1e74
Vishal <vishal@zinghr.com>
2024-04-19 18:10:50 +0530

    final

--- controllers/ALP_Controllers/generateToken.js
+++ controllers/ALP_Controllers/generateToken.js
@@ -5,27 +5,21 @@ require('dotenv').config();
 
 module.exports.GetToken = async (req, res) => {
     try {
-        const { access_Key } = req.query;
-        if (!access_Key) {
-            sendResponse(res, 400, "Please provide access_key");
-        } else if (access_Key !== "Vishal") {
-            sendResponse(res, 400, "Please provide valid access_key");
-        } else {
-            const URL = 'https://dev-uomc1epv5ow80645.us.auth0.com/oauth/token';
-            const headers = {
-                'content-type': 'application/json'
-            };
+        const URL = 'https://dev-uomc1epv5ow80645.us.auth0.com/oauth/token';
+        const headers = {
+            'content-type': 'application/json'
+        };
 
-            const data = JSON.stringify({
-                client_id: process.env.CLIENT_ID,
-                client_secret: "GlU-h4XvejgA_5QWVFvN25oyrhWePhLGCzdxt44MSfgcyjBHEad9gULw-Cr2hWw3",
-                audience: "https://alp_node.com",
-                grant_type: "client_credentials"
-            });
+        const data = JSON.stringify({
+            client_id: process.env.CLIENT_ID,
+            client_secret: "GlU-h4XvejgA_5QWVFvN25oyrhWePhLGCzdxt44MSfgcyjBHEad9gULw-Cr2hWw3",
+            audience: "https://alp_node.com",
+            grant_type: "client_credentials"
+        });
 
-            const response = await axios.post(URL, data, { headers });
-            sendResponse(res, 200, "Success", `Bearer ${response.data.access_token}`);
-        }
+        const response = await axios.post(URL, data, { headers });
+        sendResponse(res, 200, "Success", `Bearer ${response.data.access_token}`);
+
     } catch (error) {
         console.log(error)
         sendError(res, error, 500);
